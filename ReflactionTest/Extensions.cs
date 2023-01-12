5ac11a37a01e29a2ffdd33568de2231581aec610
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-01-12 19:46:01 +0400

    Add project files.

---
+++ ReflactionTest/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflactionTest
+{
+    static class Extensions
+    {
+        public static MethodInfo GetMethodInfoByName(this IEnumerable<MethodInfo> infos, string name)
+        {
+            foreach (MethodInfo item in infos)
+            {
+                if (item.Name.Equals(name, StringComparison.OrdinalIgnoreCase))
+                    return item;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ReflactionTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace ReflactionTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // RTTI - RunTime Type Identitfication
+
+            Type t = typeof(string);
+
+            string methodName = "ToLower";
+
+            MethodInfo methodInfo = 
+                t.GetMethods()
+                .GetMethodInfoByName(methodName);
+
+            string s = "HELLO";
+
+            string ls = methodInfo.Invoke(s, null).ToString();
+
+            Console.WriteLine(ls);
+        }
+    }
+}
