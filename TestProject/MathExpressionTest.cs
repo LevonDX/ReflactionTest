13ae5d3926ce315b197ef7cec38a5e0ee350ced3
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-01-14 12:41:19 +0400

    GetArgumentsUnitTest done

--- TestProject/MathExpressionTest.cs
+++ TestProject/MathExpressionTest.cs
@@ -6,7 +6,7 @@ namespace TestProject
     public class MathExpressionTest
     {
         [TestMethod]
-        public void TestGetArguments()
+        public void TestGetMethodName()
         {
             // assign
             string correct = "sqrt(100)";
@@ -21,6 +21,30 @@ namespace TestProject
             // assert
             Assert.AreEqual("sqrt", correctMethodName);
             Assert.ThrowsException<ArgumentException>(() => incorrectSolver.GetMethodName());
+        }
+
+        [TestMethod]
+        public void TestGetArguments()
+        {
+            // assign
+            string oneArg = "sqrt(100)";
+            string incorrect = "Ararat73";
+            string twoArgs = "pow(2, 3)";
+
+            MathExpressionSolver oneArgSolver = new MathExpressionSolver(oneArg);
+            MathExpressionSolver twoArgSolver = new MathExpressionSolver(twoArgs);
+
+            // act
+            object[] argsOne = oneArgSolver.GetArguments();
+            object[] argsTwo = twoArgSolver.GetArguments();
+
+            // assert
+            Assert.IsTrue(argsOne[0] is double);
+            Assert.AreEqual(argsOne[0], 100d);
+
+            Assert.AreEqual(argsTwo[0], 2d);
+            Assert.AreEqual(argsTwo[1], 3d);
+            Assert.IsTrue(argsTwo.Length == 2);
         }
     }
 }
